89834d3b2d2e2f3fa880ec228beac2bcf95022aa
FieldMarshallObvious <noahdelangel@gmail.com>
2021-10-27 00:01:40 -0500

    Added header skeleton for Pieces.h, and also fleshed out all the was needed on Board.h/Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,12 +1,21 @@
 
 #include "Board.h"
 
+// Set specified board location to specified piece
 void Board::setPos( string pos, int piece )
 {
+    // Get specified board location
+    auto it = boardLocation.find(pos);
 
+    // Set the new piece
+    it -> second = piece;
 }
 
+// Print board state to command line 
 void Board::printBoard( )
 {
-    
+    for( auto it = boardLocation.cbegin(); it != boardLocation.cend(); ++it)
+    {
+        cout << it -> first << ": " << it -> second << endl;
+    }
 }
